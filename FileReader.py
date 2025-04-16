1dccafdcb934385ad5b162ac22c2544ba8c9e738
Alex Gardenier <agardenier882@g.rwu.edu>
2025-04-16 16:41:39 -0400

    AG-9,made path handeling more flexible, also made terminal tester to test functiionality without the GUI needing to be altered.

--- FileReader.py
+++ FileReader.py
@@ -1,11 +1,10 @@
 import pandas as pd
-
+import os
 
 #new class
 class fileReader:
         
     # read the file
-
     def readSEC(name):
         #define data
         data = {
@@ -17,7 +16,7 @@ class fileReader:
         #create a dictonary for the data
         dictionary = list(data.values())
         #if the correct file type
-        if name.split(".")[2] == "SEC":
+        if name.split(".")[-1].lower() == "sec":
             #counter to skip one
             skip_one = 0
             # iterate through the file
@@ -44,10 +43,19 @@ class fileReader:
         else:
             # a bad thing happened
             raise Exception("Wrong File Type Passed")
-    def bulkReadSEC(filePath,secFileList):
-        secPath = filePath.split("/Run")[0]+"/Sections/"
+            
+    def bulkReadSEC(filePath, secFileList):
+        #Get base directory and construct sections path using proper OS path functions
+        base_dir = os.path.dirname(os.path.dirname(filePath))
+        secPath = os.path.join(base_dir, "Sections")
+        
         secList = []
         for secFile in secFileList:
-            secList.append(fileReader.readSEC(secPath+secFile))
+            secList.append(fileReader.readSEC(os.path.join(secPath, secFile)))
         return secList
-        
+
+
+
+
+# # # # needed to fix the path handling so it works on all OS
+# # # # replaced string splitting with os.path.join
--- run_parser.py
+++ run_parser.py
@@ -1,12 +1,19 @@
+import os
+
 #run parser
 def runReader(runFile):
     #inital zero
     lines = []
-    #strip the run file path
-    grpPath = runFile.split("/Run")[0]+"/Groups/"
+    #Get base directory and construct groups path using proper OS path functions
+    base_dir = os.path.dirname(os.path.dirname(runFile))
+    grpPath = os.path.join(base_dir, "Groups")
+    
     with open(runFile, 'r') as file:
         next(file)  # Skip the first title line
         for line in file:
-            lines.append(grpPath+line.rstrip())
+            lines.append(os.path.join(grpPath, line.rstrip()))
     return lines
 
+
+# # needed to fix the path handling so it works on all OS
+# # replaced string splitting with os.path.join
